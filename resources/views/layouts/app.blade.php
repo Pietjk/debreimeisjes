f687732191fcc03461e934336029efa113cd118b
Piet <pkorfmaker@gmail.com>
2021-07-30 00:44:47 +0200

    Nav/header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,8 @@
 </head>
 <body>
     <div id="app">
-        <div style="width: 100%; height: 300px; background-color:green;"></div>
+        <div class="header-img"></div>
+        {{-- <img src="{{ asset('/images/header.jpg') }}" alt="Hoofd afbeelding" height="300px" width="100%"> --}}
         <nav class="navbar sticky-top navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
@@ -26,30 +27,23 @@
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse float-end" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                      <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                      </li>
-                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Dropdown link
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                          <li><a class="dropdown-item" href="#">Action</a></li>
-                          <li><a class="dropdown-item" href="#">Another action</a></li>
-                          <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                      </li>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Ontwerpen</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Nieuws</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Over Mij</a>
+                        </li>
                     </ul>
-                  </div>
+                </div>
             </div>
         </nav>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container-fluid">
     @for ($i=0; $i<15; $i++)
         <div style="height: 100px; width: 100%; margin-bottom: 20px; background-color: purple;"></div>
     @endfor
+</div>
 @endsection
