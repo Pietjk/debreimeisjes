1e807b1440be6c2fb2acd7299c80f1267d0a97c7
Piet <pkorfmaker@gmail.com>
2021-10-15 00:13:39 +0200

    Fix route names

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -18,12 +18,12 @@
     <hr class="text-primary">
 
     <h3 class="text-primary">{{ $news->title }}@auth <a href="{{ route('post.edit', $news) }}"><i class="fas fa-pen-square ps-2"></i></a>@endauth</h3>
-    <div onclick="location.href='{{ route('news') }}'" class="card bg-primary mb-3" title="{{ route('news') }}">
+    <div onclick="location.href='{{ route('nieuws') }}'" class="card bg-primary mb-3" title="{{ route('nieuws') }}">
         <div class="row products">
             <div class="col-3 col-sm-2 col-lg-1 align-self-center"><img src="{{ asset('/images/placeholder.jpg') }}" alt="" height="75px" width="75px"></div>
             <div class="col-9 col-sm-10 col-lg-11 text-white align-self-center"><p>De grootste deken?!?!</p></div>
         </div>
     </div>
-    <a href="{{ route('news') }}">Bekijk al het nieuws hier</a>
+    <a href="{{ route('nieuws') }}">Bekijk al het nieuws hier</a>
 </div>
 @endsection
