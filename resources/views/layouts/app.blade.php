d4e99af3ff272af010cac7cb013bd29b421212e8
PietJK <37580277+PietJK@users.noreply.github.com>
2021-11-22 09:19:26 +0100

    Fix gitignore

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,11 +9,11 @@
 
     <title>{{ config('app.name', 'De Breimeisjes') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
